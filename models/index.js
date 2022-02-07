df8f9d6b161e2250da7334a384944067a28b94a7
Ava <avamsteimle@gmail.com>
2022-02-06 19:35:19 -0800

    deleted createQuery

--- models/index.js
+++ models/index.js
@@ -3,7 +3,6 @@ const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
-const { createQuery } = require('mysql2/typings/mysql/lib/Connection');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
