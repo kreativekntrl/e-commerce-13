ef8ac3c48547a275e25d20f00905d2a461e7b758
Ava <avamsteimle@gmail.com>
2022-02-07 16:46:21 -0800

    GET sinlge product testes/working

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -3,7 +3,7 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // The `/api/products` endpoint
 
-// get all products
+// GET all products
 router.get('/', async (req, res) => {
   try {
     const productData = await Product.findAll({
@@ -18,10 +18,27 @@ router.get('/', async (req, res) => {
   }
 });
 
-// get one product
-router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+// GET single product
+router.get('/:id', async (req, res) => {
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: [{
+        model: Category,
+        model: Tag, through: ProductTag,
+      }],
+    });
+
+    if (!productData) {
+      res.status(404).json({
+        message: 'No product found with that id!'
+      });
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
